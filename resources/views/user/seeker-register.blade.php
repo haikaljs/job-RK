7dc2c1b8d9fdb03127d9f2703d8affee79c2d49d
haikaljs <haikaljavascript@gmail.com>
2023-09-18 23:15:53 +0800

    Styling seeker-register

--- resources/views/user/seeker-register.blade.php
+++ resources/views/user/seeker-register.blade.php
@@ -2,45 +2,46 @@
 
 @section('content')
 
-    <div class="container" style="margin-top: 90px;">
+    <div class="container my-5">
         <div class="row">
-          
-                <div class="col-md-6 mb-3">
+                
+                        <div class="col-md-6 mx-auto">
+                            <div class="card shadow-lg">
+                                <div class="card-header bg-dark text-white p-5">
+                                    
+                                    <div class="text-center">
+                                        <h5>Looking for a job?</h5>
+                                        <h6 >Please create an account</h6>
+                                    </div>
+                                   
+                                  
+                                </div>
+                                <div class="card-body p-5">
+                                    <form>
+                                        <div class="mb-3">
+                                            <label class="form-label">Full Name</label>
+                                            <input type="name" class="form-control "  >
+                                            
+                                          </div>
+                                        <div class="mb-3">
+                                          <label class="form-label">Email address</label>
+                                          <input type="email" class="form-control "  >
+                                          
+                                        </div>
+                                        <div class="mb-3">
+                                          <label  class="form-label" >Password</label>
+                                          <input type="password" class="form-control " >
+                                        </div>
+                                      
+                                        <button type="submit" class="btn btn-dark w-100 mt-4">Submit</button>
+                                      </form>
+                                </div>
+                            </div>
+                           
+                        </div>
                    
-                    <img src="{{ asset('images/register.jpg') }}" class="img-fluid" alt="" style="border-radius: 7px;">
-                </div>
-                <div class="col-md-6 mb-5">
-                    <div class="card pt-3">
-                        <div class="text-center">
-                            <h5 class="text-muted">Looking for a job?</h5>
-                            <h6 class="text-muted">Please create an account</h6>
-                        </div>
-                        
-                        <div class="card m-4">
-                            <div class="card-header">
-                                Register
-                            </div>
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <label>Full Name</label>
-                                    <input type="text" name="name" class="form-control">
-                                </div>
-                                <div class="form-group">
-                                    <label>Email</label>
-                                    <input type="text" name="email" class="form-control">
-                                </div>
-                                <div class="form-group">
-                                    <label>Password</label>
-                                    <input type="text" name="password" class="form-control">
-                                </div>
-                                <div class="form-group mt-3">
-                                    <button class="btn btn-dark" type="submit" style="width: 100%">Register</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                  
-                </div>
+               
             
            
         </div>
