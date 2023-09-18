2a1b0c2f45e01d64dab6e3c55b9371d992004cc8
haikaljs <haikaljavascript@gmail.com>
2023-09-19 03:11:41 +0800

    Employer dashboard design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,8 +4,33 @@
 
     <div class="container my-5">
         <div class="row justify-content-center">
-            {{ auth()->user()->name }}
-            {{ auth()->user()->email }}
+            Hello, {{ auth()->user()->name }}
+            <p>Your trial will expire on {{ auth()->user()->user_trial }}</p>
+
+            <div class="col-md-3">
+                <div class="card bg-secondary">
+                    <p class="text-center mt-3 lead text-white">User Profile</p>
+                    <button class="btn btn-light d-flex align-self-end justify-content-center w-25 text-center">View</button>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card bg-primary">
+                    <p class="text-center mt-3 lead text-white">Post Job</p>
+                    <button class="btn btn-light d-flex align-self-end justify-content-center w-25 text-center">View</button>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card bg-warning">
+                    <p class="text-center mt-3 lead text-white">All Jobs</p>
+                    <button class="btn btn-light d-flex align-self-end justify-content-center w-25 text-center">View</button>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card bg-success">
+                    <p class="text-center mt-3 lead text-white">Item 4</p>
+                    <button class="btn btn-light d-flex align-self-end justify-content-center w-25 text-center">View</button>
+                </div>
+            </div>
         </div>
     </div>
 
