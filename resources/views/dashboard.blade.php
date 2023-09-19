ed1c915a854986238e9c2bdbd2e37ef3479a4b3b
haikaljs <haikaljavascript@gmail.com>
2023-09-19 10:59:49 +0800

    Show user_trial expiry messaage

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,39 +4,38 @@
 
     <div class="container my-5">
         @include('message')
-        <div class="fw-bold fs-5">
+        <div class="fw-bold fs-5 mb-3">
             Hello, {{ auth()->user()->name }}
             @if (Auth::check() && auth()->user()->user_type == 'employer')
-            <p>Your trial will expire on {{ auth()->user()->user_trial }}</p>
+            <p>Your trial {{ now()->format('Y-m-d') >  auth()->user()->user_trial ? 'was expired' : 'will expire' }} on {{ auth()->user()->user_trial }}</p>
             @endif
         </div>
         
         
         <div class="row justify-content-center">
            
-
-            <div class="col-md-3">
-                <div class="card bg-secondary">
+            <div class="col-md-3 mb-3">
+                <div class="card bg-secondary p-2">
                     <p class="text-center mt-3 lead text-white">User Profile</p>
-                    <button class="btn btn-light d-flex align-self-end justify-content-center w-25 text-center">View</button>
+                    <button class="btn btn-light text-center">View</button>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="card bg-primary">
+            <div class="col-md-3  mb-3">
+                <div class="card bg-primary p-2">
                     <p class="text-center mt-3 lead text-white">Post Job</p>
-                    <button class="btn btn-light d-flex align-self-end justify-content-center w-25 text-center">View</button>
+                    <button class="btn btn-light text-center">View</button>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="card bg-warning">
+            <div class="col-md-3  mb-3">
+                <div class="card bg-warning p-2">
                     <p class="text-center mt-3 lead text-white">All Jobs</p>
-                    <button class="btn btn-light d-flex align-self-end justify-content-center w-25 text-center">View</button>
+                    <button class="btn btn-light text-center">View</button>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="card bg-success">
+            <div class="col-md-3  mb-3">
+                <div class="card bg-success p-2">
                     <p class="text-center mt-3 lead text-white">Item 4</p>
-                    <button class="btn btn-light d-flex align-self-end justify-content-center w-25 text-center">View</button>
+                    <button class="btn btn-light text-center">View</button>
                 </div>
             </div>
         </div>
